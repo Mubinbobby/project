e85c654024d9d41934b5934d1fed4597883d8b03
Mubinbobby <bobbymubinm@gmail.com>
2024-11-07 15:03:52 +0530

    Update Login.jsx

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
 
 const AuthLogin = () => {
   return (
-    <div>Login</div>
+    <div className='bg-warning align-items-center'>
+        <h4 className='align-items-center'>Login</h4> <br />
+        <TextField
+          id="outlined-required"
+          label="Username"
+        /> <br /> <br />
+        <TextField
+          id="outlined-required"
+          label="Password"
+        />
+    </div>
   )
 }
 
